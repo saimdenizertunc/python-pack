05fee2da5680de110cb7622a4a7f178b69cd79f9
saimdenizertunc <saimdenizertunc@gmail.com>
2022-07-10 16:26:54 +0300

    Add preorder

---
+++ N-ary Tree Preorder Traversal.py
@@ -0,0 +1,22 @@
+# Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+import math
+
+
+def preorder(self, root: 'Node') -> List[int]:
+    def depth_first(node, ans):
+        ans.append(node.val)
+        for child in node.children:
+            depth_first(child, ans)
+    ans = []
+    if root == None:
+        return ans
+    depth_first(root, ans)
+    return ans
