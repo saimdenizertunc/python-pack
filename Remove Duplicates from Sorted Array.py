45f2330abb4fe83bf03e59ea2255f116dd00eabb
saimdenizertunc <saimdenizertunc@gmail.com>
2022-07-10 15:50:03 +0300

    Add Remove Duplicates, Roman to Integer and Top K Frequent Elements

---
+++ Remove Duplicates from Sorted Array.py
@@ -0,0 +1,7 @@
+def removeDuplicates(nums) -> int:
+    x = 1
+    for i in range(len(nums)-1):
+        if(nums[i] != nums[i+1]):
+            nums[x] = nums[i+1]
+            x += 1
+    return(x)
---
+++ Top K Frequent Elements.py
@@ -0,0 +1,10 @@
+def topKFrequent(nums, k):
+    freq_table = Counter(nums)
+    ans_table = freq_table.most_common()
+    ans = []
+    for key, _ in ans_table:
+        if k <= 0:
+            break
+        k -= 1
+        ans.append(key)
+    return ans
