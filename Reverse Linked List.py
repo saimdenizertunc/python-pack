6839739de6b509042acac10aa418f3cb4dd0892a
saimdenizertunc <saimdenizertunc@gmail.com>
2022-07-10 16:33:57 +0300

    Edit reverseList

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -1,14 +1,14 @@
 # Given the head of a singly linked list, reverse the list, and return the reversed list.
 
- def reverseList(head):
-      curr_node, prev, nextt = head, None, None
-       while curr_node:
-            # Making changes to the node
-            nextt = curr_node.next
-            curr_node.next = prev
+   def reverseList(head):
+        curr_node, prev, nextt = head, None, None
+           while curr_node:
+                # Making changes to the node
+                nextt = curr_node.next
+                curr_node.next = prev
 
-            # Traversing through the Linked List
-            prev = curr_node
-            curr_node = nextt
-        head = prev
+                # Traversing through the Linked List
+                prev = curr_node
+                curr_node = nextt
+            head = prev
         return head
